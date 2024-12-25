1e608709ae5a9110e3d53f80439e61a3b7530ebb
prerna008 <114279070+prerna008@users.noreply.github.com>
2024-12-25 20:36:08 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Calculator from './src/Calculator';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Calculator />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+});
+
+export default App;
